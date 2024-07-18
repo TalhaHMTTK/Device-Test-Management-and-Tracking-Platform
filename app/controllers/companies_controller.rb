11dae297eb61e55d38c7ac2daf6d80b329e9a923
TalhaHMTTK <talhahmttk2@gmail.com>
2024-07-18 17:10:20 +0500

    Deleted index action for company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,7 @@
 class CompaniesController < ApplicationController
   before_action :authenticate_user!
+
   def show
     @company = Company.find(params[:id])
-  end
-
-  def index
-    @companies = Company.all
   end
 end
