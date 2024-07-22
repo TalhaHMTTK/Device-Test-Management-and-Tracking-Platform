0620419ec7dcc1505fec45f6b9091b41482fafad
TalhaHMTTK <talhahmttk2@gmail.com>
2024-07-22 12:24:25 +0500

    Updated enum for status and added for result

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,5 +3,6 @@ class Test < ApplicationRecord
   belongs_to :company
   belongs_to :user
 
-  enum :status, { planned: 0, working: 1, completed: 2 }
+  enum :status, { scheduled: 0, in_progress: 1, completed: 2 }
+  enum :result, { pass: 0, fail: 1 }
 end
