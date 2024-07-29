d0199e13dd6228bedcd55cc4b133b249770d6b83
TalhaHMTTK <talhahmttk2@gmail.com>
2024-07-29 19:01:30 +0500

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   set_current_tenant_through_filter
   before_action :set_tenant, if: -> { current_user }
 
